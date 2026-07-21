eb6c625ee90a28343b5e1a65f036c402bf71f4a6
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2026-07-20 20:38:58 -0600

    First Commit

---
+++ src/test/java/mx/tecdesoftware/streaming_backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tecdesoftware.streaming_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
